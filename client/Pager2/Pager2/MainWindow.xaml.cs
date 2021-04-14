5e5eeed9d35bc17b5d7cbd136a8f9a82501c1fc5
Владимир <51334865+wingofnight@users.noreply.github.com>
2021-04-14 06:44:36 +0300

    Add files via upload

---
+++ client/Pager2/Pager2/MainWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Pager2
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        public void txt_bx_stabilizer()
+        {
+            if (txt_bx_login.Text == "")
+            {
+                txt_bx_login.Text = "Логин";
+            }
+            if (txt_bx_pass.Text == "")
+            {
+                txt_bx_pass.Text = "Пароль";
+            }
+            if (txt_bx_ress.Text == "")
+            {
+                txt_bx_ress.Text = "Введите Email для востановления";
+            }
+
+        }
+        private void txt_bx_pass_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            txt_bx_stabilizer();
+            if (txt_bx_pass.Text == "Пароль")
+            {
+                txt_bx_pass.Text = "";
+            }
+        }
+
+        private void txt_bx_login_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            txt_bx_stabilizer();
+            if (txt_bx_login.Text == "Логин")
+            {
+                txt_bx_login.Text = "";
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var registred = new Registred();
+            registred.Show();
+            Close();
+        }
+
+        private void btn_ress_Click(object sender, RoutedEventArgs e)
+        {
+            txt_bx_ress.Visibility = Visibility.Visible;
+            btn_email_ress.Visibility = Visibility.Visible;
+        }
+
+        private void txt_bx_ress_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            txt_bx_stabilizer();
+            if (txt_bx_ress.Text == "Введите Email для востановления")
+            {
+                txt_bx_ress.Text = "";
+            }
+        }
+
+        private void btn_accept_Click(object sender, RoutedEventArgs e)
+        {
+            Person person = new Person
+            {
+                login = txt_bx_login.Text,
+                pass = txt_bx_pass.Text,
+                protocol = "LOGIN"
+            };
+            //тут будет функция передающая данные на сервер.
+        }
+    }
+
+    public static class Server_connect{
+        static int port = 8005; // порт сервера
+        static string address = "127.0.0.1"; // адрес сервера              
+        static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+       
+        
+
+      /*  private static string Listen()
+         {   
+
+             do
+             {
+                 bytes = handler.Receive(data);
+                 builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+             }
+             while (handler.Available > 0);
+
+             handler.Shutdown(SocketShutdown.Both);
+             handler.Close();
+             return builder.ToString();
+         }*/
+
+        public static string connect(string message)
+        {          
+            try
+            {
+                byte[] data = Encoding.Unicode.GetBytes(message);               
+                socket.Connect(ipPoint);
+                socket.Send(data);//отправляем сообщение на сервер 
+               
+                data = new byte[256]; // буфер для ответа
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0; // количество полученных байт
+                do
+                {
+                    bytes = socket.Receive(data, data.Length, 0);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (socket.Available > 0);
+               // socket.Shutdown(SocketShutdown.Both);
+               // socket.Close();
+                return builder.ToString();
+            }
+            catch (System.Exception)
+            {
+                return "Нет подключения к серверу";
+            }        
+            
+        }
+    }
+    
+}
---
+++ client/Pager2/Pager2/Registred.xaml.cs
@@ -0,0 +1,167 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Text.Json;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Pager2
+{
+
+    public partial class Registred : Window
+    {
+        Person_Registred person = new Person_Registred();
+        private string login_reg = "Логин";
+        private string pass_reg = "Пароль";
+        private string pass_2_reg = "Повторите пароль";
+        private string email_reg = "email";        
+
+        public Registred()
+        {
+            InitializeComponent();
+        }
+        public void txt_bx_stabilizer()
+        {
+            if (txt_bx_login_reg.Text == "")
+            {
+                txt_bx_login_reg.Text = login_reg;
+            }
+            
+            if (txt_bx_pass_reg.Text == "")
+            {
+                txt_bx_pass_reg.Text = pass_reg;
+            }
+            
+            if (txt_bx_pass_reg_2.Text == "")
+            {
+                txt_bx_pass_reg_2.Text = pass_2_reg;
+            }
+            
+            if (txt_bx_mail_reg.Text == "")
+            {
+                txt_bx_mail_reg.Text = email_reg;
+            }          
+
+        }
+
+        private void txt_bx_login_reg_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            txt_bx_stabilizer();
+            if (txt_bx_login_reg.Text == login_reg)
+            {
+                txt_bx_login_reg.Text = "";
+            }
+           
+        }
+
+        private void txt_bx_pass_reg_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txt_bx_stabilizer();
+            if (txt_bx_pass_reg.Text == pass_reg)
+            {
+                txt_bx_pass_reg.Text = "";
+            }                    
+        }
+
+        private void txt_bx_pass_reg_2_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            txt_bx_stabilizer();
+            if (txt_bx_pass_reg_2.Text == pass_2_reg)
+            {
+                txt_bx_pass_reg_2.Text = "";
+            }                      
+        }
+
+        private void txt_bx_mail_reg_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            txt_bx_stabilizer();
+            if (txt_bx_mail_reg.Text == email_reg)
+            {
+                txt_bx_mail_reg.Text = "";
+            }                        
+        }
+      
+        private void btn_reg_Click(object sender, RoutedEventArgs e)
+        {
+            
+            person.protocol = "REGISTRED";
+            person.mail = txt_bx_mail_reg.Text;
+            person.login = txt_bx_login_reg.Text;
+            person.pass = txt_bx_pass_reg.Text;
+            person.mail_accepr = chk_bx_mail.IsChecked.Value;
+
+            if (check_login() == true)
+            {
+                string json = JsonSerializer.Serialize<Person_Registred>(person);
+                try
+                {                    
+                    txt_blk_server_down.Foreground = Brushes.Red;
+                    txt_blk_server_down.Text = Server_connect.connect(json); 
+                }
+                catch (System.Exception)
+                {
+                    txt_blk_server_down.Foreground = Brushes.Red;
+                    txt_blk_server_down.Text = "Сервер не отвечает";
+                }
+            }          
+            
+        }
+
+        private bool check_login()
+        {
+            if (txt_bx_login_reg.Text == "" || txt_bx_login_reg.Text == "Логин")
+            {
+                txt_blk_lgn_access.Text = "Введите Логин!";
+                txt_blk_lgn_access.Foreground = Brushes.Red;
+                return false;
+            }
+            else
+            {
+                txt_blk_lgn_access.Text = "Логин подходит!";
+                txt_blk_lgn_access.Foreground = Brushes.Green;
+                return check_password();
+            }
+        }
+        private bool check_password()
+        {            
+
+            if (txt_bx_pass_reg.Text.Length >= 6)
+                {
+                    if (txt_bx_pass_reg.Text == txt_bx_pass_reg_2.Text)
+                    {
+                        person.pass = txt_bx_pass_reg.Text;
+                        txt_blk_pass_access.Text = "Пароли совпадают!";
+                        txt_blk_pass_access.Foreground = Brushes.Green;
+                        return true;
+                    }
+                    else
+                    {
+                        txt_blk_pass_access.Text = "Пароли не совпадают!";
+                        txt_blk_pass_access.Foreground = Brushes.Red;
+                        return false;
+                    }
+                }
+                else
+                {
+                    txt_blk_pass_access.Text = "В пароле должно быть не меньше 6 символов!";
+                    txt_blk_pass_access.Foreground = Brushes.Red;
+                    return false;
+                }            
+        }
+            
+       }
+
+    public class Person
+    {
+        public string protocol { get; set; }
+        public string login { get; set; }
+        public string pass { get; set; }
+    }
+    public class Person_Registred:Person   {    
+       
+        public string mail { get; set; }
+        public bool mail_accepr { get; set; }
+    }
+
+}
